a83b948b40c44346dab834d4da2a9568cdc5bb52
Arun <arun@arun.blog>
2024-09-25 21:08:20 -0400

    chore: remove boilerplace workers comments

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,3 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
 import { GetObjectCommand, RequestProgress, S3Client } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import Replicate, { validateWebhook } from "replicate";
